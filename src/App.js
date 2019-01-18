caa75c6e909fe4f96fceab2c7cdb763468efc80e
Nissa Wollum <nwollum@bombora.com>
2019-01-17 17:01:05 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,27 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Letter from './Letter';
+
+const letter_groups = {
+  vowels: ['a', 'e', 'i', 'o', 'u'],
+  consonants: ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+};
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      letter: this.getRandom(letter_groups.vowels)
+    }
+    this.randomBlend = this.randomBlend.bind(this)
+    this.cycleVowels = this.cycleVowels.bind(this)
+    this.cycleConsonants = this.cycleConsonants.bind(this)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <Letter name={this.state.letter}></Letter>
+          <div style={{display:"flex",justifyContent: "center"}}>
+            <div style={{border:"1px solid salmon",marginRight:"5px",padding:"10px"}}>
+              <h3>Single Letters</h3>
+              <button onClick={(e) => this.changeLetter(letter_groups.vowels)}>Get a new vowel</button>
+              <button onClick={(e) => this.changeLetter(letter_groups.consonants)}>Get a new consonant</button>
+            </div>
+            <div style={{border:"1px solid lightgray",marginLeft:"5px",padding:"10px"}}>
+              <h3>Vowel Blends</h3>
+              <button onClick={this.randomBlend}>Random Blend</button>
+              <button onClick={this.cycleVowels}>Cycle Vowels</button>
+              <button onClick={this.cycleConsonants}>Cycle Consonants</button>
+            </div>
+          </div>
+          <h5>Click the letter to switch between upper and lower case!</h5>
         </header>
       </div>
     );
+  }
+  randomBlend = () => {
+    this.setState(state => ({
+      letter: `${this.getRandom(letter_groups.consonants)}${this.getRandom(letter_groups.vowels)}`
+    }))
+  }
+
+  cycleVowels = () => {
+    let vowel = this.state.letter[this.state.letter.length-1];
+    let consonant = this.state.letter.length === 2 ? this.state.letter[0] : "";
+    
+    this.setState(state => ({
+      letter: `${consonant}${this.getNextLetter(letter_groups.vowels, vowel)}`
+    }))
+  }
+
+
+  cycleConsonants = () => {
+    let vowel = this.state.letter.length === 2 ? this.state.letter[1] : "";
+    let consonant = this.state.letter[0];
+
+    this.setState(state => ({
+      letter: `${this.getNextLetter(letter_groups.consonants, consonant)}${vowel}`
+    }))
+  }
+
+  getNextLetter = (list, letter) => {
+    let currIdx = list.indexOf(letter);
+    if (currIdx !== -1 && currIdx < list.length - 1) {
+      return list[++currIdx];
+    } else {
+      return list[0];
+    }
+  }
+
+  getRandom = (list) => {
+    let idx = Math.floor(Math.random() * list.length);
+    return list[idx];
+  }
+
+  changeLetter = (list) => {
+    this.setState(state => ({
+      letter: this.getRandom(list)
+    }));
   }
 }
 
---
+++ src/Letter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+const CASE = ['lowercase','uppercase'];
+
+class Letter extends Component {
+  constructor(){
+    super();
+    this.state = {
+      case: 0
+    };
+    this.changeCase = this.changeCase.bind(this);
+  }
+
+  render(){
+    return (
+      <h1 
+      style={{ textTransform: `${CASE[this.state.case]}`, fontSize:"99px", cursor:"pointer"}} 
+      onClick={this.changeCase}
+      >
+      { this.props.name }
+      </h1>
+    )
+  }
+
+  changeCase = () => {
+    this.setState(state => ({
+      case: 1 - state.case
+    }));
+  }
+}
+
+
+export default Letter;
